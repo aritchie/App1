e9e4de6761aa765c9b65489aa2f60ed5bd5a2508
Allan Ritchie <allan.ritchie@gmail.com>
2021-03-31 20:18:38 -0400

    Add project files.

---
+++ App1/App1.iOS/AppDelegate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using Shiny;
+
+namespace App1.iOS
+{
+
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            this.ShinyFinishedLaunching(new MyShinyStartup());
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+
+
+        public override void PerformFetch(UIApplication application, Action<UIBackgroundFetchResult> completionHandler)
+            => this.ShinyPerformFetch(completionHandler);
+
+        public override void HandleEventsForBackgroundUrl(UIApplication application, string sessionIdentifier, Action completionHandler)
+            => this.ShinyHandleEventsForBackgroundUrl(sessionIdentifier, completionHandler);
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+            => this.ShinyRegisteredForRemoteNotifications(deviceToken);
+
+        public override void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
+            => this.ShinyFailedToRegisterForRemoteNotifications(error);
+
+        public override void DidReceiveRemoteNotification(UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
+            => this.ShinyDidReceiveRemoteNotification(userInfo, completionHandler);
+    }
+}
+
+
+
+
+
+
+
+
+
+//[assembly: ShinyApplication(
+//    ShinyStartupTypeName = "App1.MyShinyStartup",
+//    XamarinFormsAppTypeName = "App1.App"
+//)]
